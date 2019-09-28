17d3441ecc5eb7b60e60ed2dd2a5a11cbd48b81c
Толянчик <tolyara1961@gmail.com>
2019-09-29 02:14:17 +0300

    Revert "Bad commit with pushing"
    This reverts commit 2faf361c28b857f5864a66c0313fcf1380a7c791.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,5 @@ public class Main {
     public static void main(String[] args) {
         DeMorganLaws dm = new DeMorganLaws();
         dm.checkDeMorganLaws();
-        try {
-			throw new Throwable();
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
     }
 }
