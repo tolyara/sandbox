58cad681093d79ab9f162b8be472e923c16080b7
Толянчик <tolyara1961@gmail.com>
2019-09-29 02:07:27 +0300

    Revert "Bad commit"
    This reverts commit d63d6cba1c9b759961ae7728f9a294dbb461ccab.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 
 public class Main {
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
         DeMorganLaws dm = new DeMorganLaws();
         dm.checkDeMorganLaws();
-        throw new Throwable();
     }
 }
