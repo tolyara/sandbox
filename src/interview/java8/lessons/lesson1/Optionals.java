1562073b501e31c00b7db26333c5de8214a0fa91
Толянчик <tolyara1961@gmail.com>
2020-05-02 00:26:45 +0300

    optional, sum of array

--- src/interview/java8/lessons/lesson1/Optionals.java
+++ src/interview/java8/lessons/lesson1/Optionals.java
@@ -12,11 +12,29 @@ public class Optionals {
 //		Optional<String> optional = Optional.empty();
 //		Optional<String> optional2 = Optional.ofNullable(null);
 
-		Optional<String> optional = getOptional();
-		if (optional.isPresent()) {
-			System.out.println(optional.get());
-		}
+//		Optional<String> optional = getOptional();
+//		if (optional.isPresent()) {
+//			System.out.println(optional.get());
+//		}
+		
+		mapExample();	
 
+	}
+	
+	private static void  orElseExample() {
+		Optional<String> optional = Optional.of("John");
+		System.out.println(optional.orElse("blank"));
+		
+		Optional<String> optional2 = Optional.ofNullable(null);
+		System.out.println(optional2.orElse("blank"));
+	}
+	
+	private static void  mapExample() {
+		Optional<String> optional = Optional.of("John");
+		System.out.println(optional.map((x) -> x.toUpperCase()).orElseThrow());
+		
+		Optional<String> optional2 = Optional.of("John");
+		System.out.println(optional2.flatMap((x) -> Optional.of("Sally")).orElseThrow());
 	}
 
 	private static Optional<String> getOptional() {
