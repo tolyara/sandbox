6b17338d53c318bd978aad7e2fafc9b361aab4f8
Anatolii <anatolii.melchenko@academysmart.com>
2023-02-17 20:36:42 +0200

    interpret main;

--- src/patterns/behavioral/interpreter/InterpreterRunner.java
+++ src/patterns/behavioral/interpreter/InterpreterRunner.java
@@ -3,22 +3,21 @@ package patterns.behavioral.interpreter;
 public class InterpreterRunner {
 	
 	public static void main(String[] args) {
-		
 		Expression isJava = getJavaExpression();
 		Expression isJavaEE = getJavaEEExpression();
 
-		System.out.println("Does developer know Java Core: " + isJava.interpret("Java"));
-		System.out.println("Does developer know Java Core: " + isJava.interpret("Java Core"));
+		System.out.println("Does developer know Java Core: " + isJava.interpret("Java")); // true
+		System.out.println("Does developer know Java Core: " + isJava.interpret("Core")); // true
 		System.out.println();
 
-		System.out.println("Does developer know Java EE: " + isJavaEE.interpret("Java"));
-		System.out.println("Does developer know Java EE: " + isJavaEE.interpret("Spring"));
-		System.out.println("Does developer know Java EE: " + isJavaEE.interpret("Java Spring"));
+		System.out.println("Does developer know Java EE: " + isJavaEE.interpret("Java Maven SQL")); // false
+		System.out.println("Does developer know Java EE: " + isJavaEE.interpret("Spring Hibernate Mongo")); // false
+		System.out.println("Does developer know Java EE: " + isJavaEE.interpret("Java Kafka Spring")); // true
 	}
 	
 	public static Expression getJavaExpression() {
 		Expression java = new TerminalExpression("Java");
-		Expression javaCore = new TerminalExpression("Java Core");
+		Expression javaCore = new TerminalExpression("Core");
 		return new OrExpression(java, javaCore);
 	}
 	
