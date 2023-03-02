4d634a163093b6f21e11cd2079fca1ceda411da9
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-03 00:39:58 +0200

    refactor 5;

--- src/interview/performance/CollectionsPerformance.java
+++ src/interview/performance/CollectionsPerformance.java
@@ -8,9 +8,9 @@ import java.util.function.UnaryOperator;
 
 public class CollectionsPerformance {
 
-//    private static final int iterations = 2_000_000;
+    private static final int iterations = 2_000_000;
 //    private static final int iterations = 200_000;
-    private static final int iterations = 20_000;
+//    private static final int iterations = 20_000;
 
     private static final String str = "Hello";
 
@@ -22,7 +22,10 @@ public class CollectionsPerformance {
     };
 
     public static void main(String[] args) {
-        testListsRemoveFromBegin();
+//        testListsInsertToBegin();
+        testListsInsertToEnd();
+
+//        testListsRemoveFromBegin();
 //        testListsRemoveFromEnd();
     }
 
@@ -107,28 +110,19 @@ public class CollectionsPerformance {
     }
 
     /*
-        ArrayList wins
+        ArrayList has a little advantage
     */
     private static void testListsInsertToEnd() {
         // iterations - 2_000_000
         // linked : 99 107 119 etc.
         //  array : 83 90  85
 
-        long start = System.currentTimeMillis();
-        List<String> linkedList = new LinkedList<>();
-        for (int i = 0; i < iterations; i++) {
-            linkedList.add(str);
-        }
-        long finish = System.currentTimeMillis();
-        System.out.println("linked : " + (finish - start));
-
-        start = System.currentTimeMillis();
-        List<String> arrayList = new ArrayList<>();
-        for (int i = 0; i < iterations; i++) {
-            arrayList.add(str);
-        }
-        finish = System.currentTimeMillis();
-        System.out.println("array : " + (finish - start));
+        Consumer<List<String>> inserterToEnd = (list) -> {
+            for (int i = 0; i < iterations; i++) {
+                list.add(str);
+            }
+        };
+        testLists(new LinkedList<>(), new ArrayList<>(), list -> list, inserterToEnd);
     }
 
 }
