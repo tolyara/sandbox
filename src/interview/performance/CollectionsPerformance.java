8db4cb9752ebd328dcc50de3b335e16ecc77ced3
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-02 22:55:19 +0200

    testListsInsertToBegin;

--- src/interview/performance/CollectionsPerformance.java
+++ src/interview/performance/CollectionsPerformance.java
@@ -6,14 +6,45 @@ import java.util.List;
 
 public class CollectionsPerformance {
 
-    private static final int iterations = 2_000_000;
+//    private static final int iterations = 2_000_000;
+    private static final int iterations = 200_000;
+
     private static final String str = "Hello";
 
     public static void main(String[] args) {
-        testLists();
+        testListsInsertToBegin();
     }
 
-    private static void testLists() {
+    private static void testListsInsertToBegin() {
+        // iterations - 20_000
+        // linked : 4   3   4    etc.
+        // array : 50  47  47
+
+        // iterations - 200_000
+        // linked : 9
+        // array : 5258
+
+        long start = System.currentTimeMillis();
+        List<String> linkedList = new LinkedList<>();
+//        linkedList.add(str);
+        for (int i = 0; i < iterations; i++) {
+            linkedList.add(0, str);
+        }
+        long finish = System.currentTimeMillis();
+        System.out.println("linked : " + (finish - start));
+
+        start = System.currentTimeMillis();
+        List<String> arrayList = new ArrayList<>();
+//        linkedList.add(str);
+        for (int i = 0; i < iterations; i++) {
+            arrayList.add(0, str);
+        }
+        finish = System.currentTimeMillis();
+        System.out.println("array : " + (finish - start));
+    }
+
+    private static void testListsInsertToEnd() {
+        // iterations - 2_000_000
         // linked : 99 107 119 etc.
         //  array : 83 90  85
 
