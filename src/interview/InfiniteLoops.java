7c11c9b142d8d8fe7682fd3f4278d1011f0fc980
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-22 18:09:03 +0200

    InfiniteLoops;

---
+++ src/interview/InfiniteLoops.java
@@ -0,0 +1,11 @@
+package interview;
+
+public class InfiniteLoops {
+
+    public static void main(String[] args) {
+//        while (true) {}
+//        do {} while (1 == 1);
+//        for (;;);
+    }
+
+}
