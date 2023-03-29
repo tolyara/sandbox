95c389e06c2070779800b36dae99b6724be2754b
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-29 14:39:33 +0300

    JEP_425_Virtual_Threads;

--- src/interview/java19/Java19.java
+++ src/interview/java19/Java19.java
@@ -1,11 +1,16 @@
 package interview.java19;
 
+import java.util.concurrent.*;
+
 public class Java19 {
 
     record Point(int x, int y) {};
 
-    public static void main(String[] args) {
-        JEP_405_Record_Patterns();
+    private static final int iterations = 100_000;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        JEP_405_Record_Patterns();
+//        JEP_425_Virtual_Threads();
     }
 
     private static void JEP_405_Record_Patterns() {
@@ -21,6 +26,31 @@ public class Java19 {
 //        if (point instanceof Point(int xx, int yy)) {
 //            System.out.println(xx + yy);
 //        }
+    }
+
+    private static void JEP_425_Virtual_Threads() throws ExecutionException, InterruptedException {
+        // old, 1024 kb and each stack
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        // new, 2 kb and lightweight stack
+        ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+
+        Runnable task = () -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+                System.out.println("Thread done. ");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        for (int i = 0; i < iterations; i++) {
+            Future<?> result = executorService.submit(task);
+            result.get();
+
+//            executorService.submit(task);
+        }
+
     }
 
 }
