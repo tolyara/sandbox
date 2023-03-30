babd414c33c939dc4dbd2ce3301fa204a7e85251
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-30 21:57:44 +0300

    JEP_409_SealedClasses;

--- src/interview/java19/Java17.java
+++ src/interview/java19/Java17.java
@@ -10,8 +10,14 @@ public class Java17 {
 //        JEP_406_PatternMatchingForSwitch("Hello");
 //        JEP_406_PatternMatchingForSwitch(123);
 //        JEP_406_PatternMatchingForSwitch(new MyObject("test", 10));
+
+        JEP_409_SealedClasses();
     }
 
+    /*
+        TODO: Scanner, null
+        https://www.youtube.com/watch?v=9yZ7CQzjHSQ&ab_channel=Jetbulb
+     */
 //    private static void JEP_406_PatternMatchingForSwitch(Object o) {
 //        switch (o) {
 //            case String s -> System.out.println("String : " + s);
@@ -19,6 +25,13 @@ public class Java17 {
 //            default -> System.out.println("Object : " + o);
 //        }
 //    }
+
+    /*
+        Sealed interfaces/classes used to restrict not desirable (unwanted) inheritance
+     */
+    private static void JEP_409_SealedClasses() {
+//        new DoubleTriangle().draw();
+    }
 
 }
 
@@ -42,5 +55,21 @@ class MyObject {
 
 }
 
-
+//sealed interface Shape permits Triangle, Qube {
+//    void draw();
+//}
+//
+//non-sealed interface Qube extends Shape {}
+//
+//interface DoubleQube extends Qube {}
+//
+//sealed interface Triangle extends Shape permits DoubleTriangle {}
+//
+////final class DoubleTriangle implements Triangle {
+//non-sealed class DoubleTriangle implements Triangle {
+//    @Override
+//    public void draw() {
+//        System.out.println("DoubleTriangle is not-sealed");
+//    }
+//}
 
