0a77ac685ce548286986812270c601f52b16aa1a
Anatolii <anatolii.melchenko@academysmart.com>
2023-04-22 16:12:59 +0300

    testConstructorWorkaround;

--- src/interview/CompilationDemo.java
+++ src/interview/CompilationDemo.java
@@ -6,6 +6,10 @@ public class CompilationDemo {
 
     }
 
+    private static void testConstructorWorkaround() {
+        Child1 child1 = new Child1();
+    }
+
 }
 
 class Parent2 {
@@ -24,4 +28,23 @@ class Child2 extends Parent2 {
         return 2;
     }
 
+}
+
+/* ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+class Parent1 {
+
+    private String name;
+
+    public Parent1(String name) {
+        this.name = name;
+    }
+}
+
+class Child1 extends Parent1 {
+
+    public Child1() {
+        // workaround for parent constructor, otherwise it will be a compilation error : There is no default constructor available in 'interview.Parent1'
+        super(null);
+    }
 }
