ad7b590c70d0e48c7fc516056dd0dd29db963540
Anatolii <anatolii.melchenko@academysmart.com>
2023-05-25 01:54:09 +0300

    task4;

--- src/interview/livecoding/JuniorTasks.java
+++ src/interview/livecoding/JuniorTasks.java
@@ -1,10 +1,16 @@
 package interview.livecoding;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class JuniorTasks {
 
     public static void main(String[] args) {
 //        task2();
-        task3();
+//        task3();
+        task4();
     }
 
     private static void task1() {
@@ -26,11 +32,29 @@ public class JuniorTasks {
         OurDemoClass ourDemoClass = new OurDemoClass();
     }
 
+    private static void task4() {
+        List<String> stringList = new ArrayList<>();    // ConcurrentModificationException
+//        List<String> stringList = new CopyOnWriteArrayList<>();    // After [one, one and a half, two, two and a half, three and a half, three]
+        stringList.add("one");
+        stringList.add("one and a half");
+        stringList.add("two");
+        stringList.add("two and a half");
+        stringList.add("three and a half");
+
+        System.out.println("Before " + stringList);
+        Iterator<String> stringIterator = stringList.iterator();
+        while (stringIterator.hasNext()) {
+            String next = stringIterator.next();
+            if (next.equals("two and a half")) {
+                stringList.add("three");
+            }
+        }
+        System.out.println("After " + stringList);
+    }
+
     private static void bar(String m) {
         m += " World!";
     }
-
-
 
 }
 
