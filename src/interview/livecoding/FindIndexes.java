bfae9569f347017c60dc7c8f011740a6a4fcf8f3
Anatolii <anatolii.melchenko@academysmart.com>
2023-05-25 01:13:00 +0300

    FindIndexes works;

--- src/interview/livecoding/FindIndexes.java
+++ src/interview/livecoding/FindIndexes.java
@@ -10,7 +10,11 @@ public class FindIndexes {
 //        List<Integer> list = List.of(-4, 0, 2, null, 17, 22, null, 2, 2, 17);     // NPE
         List<Integer> list = Arrays.asList(-4, 0, 2, null, 17, 22, null, 2, 2, 17);
 
-        List<Integer> result = findIndexes(2, list);
+//        List<Integer> result = findIndexes(2, null);
+
+        List<Integer> result = findIndexes(null, list);
+//        List<Integer> result = findIndexes(2, list);
+//        List<Integer> result = findIndexes(17, list);
 
         System.out.println(result);
     }
@@ -18,7 +22,14 @@ public class FindIndexes {
     public static List<Integer> findIndexes(Integer value, List<Integer> source) {
         List<Integer> result = new ArrayList<>();
 
-
+        if (source != null) {
+            for (int i = 0; i < source.size(); i++) {
+                Integer e = source.get(i);
+                if (value == null && e == null || e != null && e.equals(value)) {
+                    result.add(i);
+                }
+            }
+        }
 
         return result;
     }
