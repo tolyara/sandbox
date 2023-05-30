66aff96933eeb7071a1fe652e362da0e72124de5
Anatolii <anatolii.melchenko@academysmart.com>
2023-05-30 17:02:43 +0300

    LongestCommonPrefix fix;

--- src/interview/livecoding/LongestCommonPrefix.java
+++ src/interview/livecoding/LongestCommonPrefix.java
@@ -11,9 +11,11 @@ public class LongestCommonPrefix {
                 "", new String[]{"dog", "racecar", "car"},
                 "java", new String[]{"java_9", "java", "java_11", "java_17"},
                 "windows", new String[]{"windows", "windows_10", "windows_11"},
+                "win", new String[]{"windows", "windows_10", "win_11"},
                 "duplicate", new String[]{"duplicate", "duplicate"},
                 "Passed", new String[]{"Passed"}
 
+//                "fl", new String[]{"flower", "flow", "flight"}
 //                "java", new String[]{"java_9", "java", "java_11", "java_17"}
                 );
         bundle.forEach((k, v) -> {
@@ -26,7 +28,7 @@ public class LongestCommonPrefix {
         StringBuilder result = new StringBuilder();
 
         int minLength = values[0].length();
-        for (int i = 1; i < values.length - 1; i++) {
+        for (int i = 1; i < values.length; i++) {
             if (values[i].length() < minLength) {
                 minLength = values[i].length();
             }
@@ -34,10 +36,10 @@ public class LongestCommonPrefix {
 //        System.out.println(minLength);
 
         if (minLength > 0) {
-            for (int i = 0; i < minLength; i++) {
+            for (int i = 0; i < minLength; i++) {   // char's index loop
                 char c = values[0].charAt(i);
                 boolean matched = true;
-                for (int j = 1; j < values.length - 1; j++) {
+                for (int j = 1; j < values.length; j++) {
 //                    System.out.println(values[j] + " " + i + " " + j);
                     if (values[j].charAt(i) != c) {
                         matched = false;
@@ -45,8 +47,10 @@ public class LongestCommonPrefix {
                     }
                 }
                 if (matched) {
+//                    System.out.println("matched -> " + c);
                     result.append(c);
                 } else {
+//                    System.out.println("not matched");
                     break;
                 }
             }
