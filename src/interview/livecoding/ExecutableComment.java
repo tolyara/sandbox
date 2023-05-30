63df3150789398746ea4e9bf0afbbc13b977f87f
Anatolii <anatolii.melchenko@academysmart.com>
2023-05-30 23:50:35 +0300

    ExecutableComment;

---
+++ src/interview/livecoding/ExecutableComment.java
@@ -0,0 +1,10 @@
+package interview.livecoding;
+
+public class ExecutableComment {
+
+    public static void main(String[] args) {
+        // комментарий ниже будет выполнен!
+        // \u000d System.out.println("comment executed");
+    }
+
+}
