7dc0753035a7c6402359d8b03e26359c993ec85d
Anatolii <anatolii.melchenko@academysmart.com>
2023-06-09 01:03:54 +0300

    mergeSortOfTwoSortedArrays();

--- src/interview/livecoding/MergeSortOfTwoArrays.java
+++ src/interview/livecoding/MergeSortOfTwoArrays.java
@@ -1,9 +1,26 @@
 package interview.livecoding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MergeSortOfTwoArrays {
 
     public static void main(String[] args) {
-        initBlocksExample();
+//        initBlocksExample();
+//        genericsExample();
+        mergeSortOfTwoArrays();
+    }
+
+    private static void genericsExample() {
+        List<? extends Number> numbers = new ArrayList<>();
+//        numbers.add(1);   // compile error
+
+        List<? super Number> numbers2 = new ArrayList<>();
+        numbers2.add(1);
+        numbers2.add(1.5);
+        System.out.println(numbers2);
     }
 
     private static void initBlocksExample() {
@@ -15,15 +32,30 @@ public class MergeSortOfTwoArrays {
     private static void mergeSortOfTwoArrays() {
         Integer[] arr1 = new Integer[] {1, 3, 5};
         Integer[] arr2 = new Integer[] {2, 4, 5};
-        System.out.println(mergeSortOfTwoArrays(arr1, arr2));
+
+        Integer[] result = mergeSortOfTwoSortedArrays(arr1, arr2);
+        System.out.print("[ ");
+        for (Integer integer : result) {
+            System.out.print(integer + " ");
+        }
+        System.out.print("]");
     }
 
-    private static Integer[] mergeSortOfTwoArrays(Integer[] arr1, Integer[] arr2) {
-        Integer[] result = new Integer[arr1.length];
+    private static Integer[] mergeSortOfTwoSortedArrays(Integer[] arr1, Integer[] arr2) {
+        List<Integer> result = new ArrayList<>();
 
-        //
+        // easy solution using Collections.sort(...)
+        Collections.addAll(result, arr1);
+        result.addAll(Arrays.asList(arr2));
+        Collections.sort(result);
 
-        return result;
+        Integer[] resultArr = new Integer[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resultArr[i] = result.get(i);
+        }
+        return resultArr;
+
+        // TODO use direct merge function
     }
 
 }
