0bf92e3f4d351f448fbb41b503ca29b66e727bc1
Anatolii <anatolii.melchenko@academysmart.com>
2023-06-11 23:28:13 +0300

    isPalindromeRecursive;

--- src/interview/livecoding/proglib/Palindrome.java
+++ src/interview/livecoding/proglib/Palindrome.java
@@ -3,12 +3,14 @@ package interview.livecoding.proglib;
 public class Palindrome {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("qwewq"));
-        System.out.println(isPalindrome("qwewe"));
+//        System.out.println(isPalindrome("qwewq"));
+//        System.out.println(isPalindrome("qwewe"));
 
 //        System.out.println(isPalindrome("qweewq"));
 //        System.out.println(isPalindrome("qweewe"));
 
+        System.out.println(isPalindromeRecursive("arozaupalanalapuazora"));
+        System.out.println(isPalindromeRecursive("test"));
     }
 
     private static boolean isPalindrome(String s) {
@@ -22,6 +24,21 @@ public class Palindrome {
             }
         }
         return result;
+    }
+
+    private static boolean isPalindromeRecursive(String s) {
+        if (s.length() == 1) {
+            return true;
+        } else {
+            if (s.substring(0, 1).equals(s.substring(s.length() - 1))) {
+                if (s.length() == 2) {
+                    return true;
+                }
+                return isPalindromeRecursive(s.substring(1, s.length() - 1));
+            } else {
+                return false;
+            }
+        }
     }
 
 }
