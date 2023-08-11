3783289b3c13a56d702f9b63e867dc2068f22601
Anatolii <anatolii.melchenko@academysmart.com>
2023-08-12 01:09:35 +0300

    VisitCounterMain 2;

--- src/interview/livecoding/VisitCounterMain.java
+++ src/interview/livecoding/VisitCounterMain.java
@@ -5,14 +5,32 @@ import java.util.*;
 public class VisitCounterMain {
 
     public static void main(String[] args) {
+        Map<String, UserStats> userStatsMap = new HashMap<>();
+        userStatsMap.put("1", new UserStats(Optional.of(11L)));
+        userStatsMap.put("2", new UserStats(Optional.of(12L)));
 
+        Map<String, UserStats> userStatsMap2 = new HashMap<>();
+        userStatsMap2.put("3", new UserStats(Optional.of(13L)));
+        userStatsMap2.put("4", new UserStats(Optional.of(14L)));
+        userStatsMap2.put("5", new UserStats(Optional.of(15L)));
+
+//        Object[] arr = new Object[2];
+//        Map<String, UserStats>[] arr = new Map<String, UserStats>[2];
+
+//        List<Map<String, UserStats>> list = Arrays.asList(userStatsMap, userStatsMap2);
+
+//        new VisitCounter().count(arr);
     }
 
 }
 
 class VisitCounter {
 
-    Map<Long, Long> count(Map<String, UserStats>... visits) {
+    /*
+        Key - user id
+        Value - count of visits of
+     */
+    public Map<Long, Long> count(Map<String, UserStats>... visits) {
         if (visits == null || visits.length == 0) {
             return Collections.emptyMap();
         }
@@ -46,6 +64,10 @@ class VisitCounter {
 class UserStats {
 
     private Optional<Long> visitCount;
+
+    public UserStats(Optional<Long> visitCount) {
+        this.visitCount = visitCount;
+    }
 
     public Optional<Long> getVisitCount() {
         return visitCount;
