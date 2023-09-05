0b56bef78abdf39ede22829193b26697049ff66b
Anatolii <anatolii.melchenko@academysmart.com>
2023-09-06 00:43:08 +0300

    getMaximumProfit_2;

--- src/interview/livecoding/leetcode/BestTimeToBuyAndSellStock.java
+++ src/interview/livecoding/leetcode/BestTimeToBuyAndSellStock.java
@@ -35,23 +35,46 @@ public class BestTimeToBuyAndSellStock {
 //        int[] prices = {7, 1, 5, 3, 6, 4};
 //        int[] prices = {7, 6, 4, 3, 1};
 
-//        int[] prices = {3, 2, 4, 1};
-        int[] prices = {};
+        int[] prices = {3, 2, 4, 1};
+//        int[] prices = {};
 
-        System.out.println(getMaximumProfit(prices));
+        System.out.println(getMaximumProfit_2(prices));
     }
 
+    /*
+        Time complexity: O(n)O(n)O(n)
+        The algorithm iterates through the array of stock prices once, performing constant-time operations at each step.
+        Therefore, the time complexity is linear in the size of the input array.
+
+        Space complexity: O(1)O(1)O(1)
+        The algorithm uses a constant amount of extra space to store variables like min_price and maxprof.
+        The space complexity remains constant regardless of the size of the input array.
+     */
     private static int getMaximumProfit(int[] prices) {
         int result = 0;
 
         for (int i = 0; i < prices.length; i++) {
             for (int j = i + 1; j < prices.length; j++) {
-                // prices[j] > prices[i] &&
                 int diff = prices[j] - prices[i];
                 if (diff > result) {
                     result = diff;
                 }
             }
+        }
+        return result;
+    }
+
+    private static int getMaximumProfit_2(int[] prices) {
+        int minPrice = prices[0];
+        int result = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            System.out.println(prices[i] +  " - " + minPrice  + " - " + result);
+            result = Math.max(result, prices[i] - minPrice);
+            System.out.println(prices[i] +  " - " + minPrice  + " - " + result);
+            minPrice = Math.min(prices[i], minPrice);
+            System.out.println(prices[i] +  " - " + minPrice  + " - " + result);
+            System.out.println();
         }
         return result;
     }
