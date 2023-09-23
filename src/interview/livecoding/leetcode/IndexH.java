45fbf637410aa4d36171ae186942eafe374bdefb
Anatolii <anatolii.melchenko@academysmart.com>
2023-09-23 14:45:08 +0300

    getHIndex2;

--- src/interview/livecoding/leetcode/IndexH.java
+++ src/interview/livecoding/leetcode/IndexH.java
@@ -40,8 +40,8 @@ public class IndexH {
     private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-//        runTestCases();
-        checkPerformance();
+        runTestCases();
+//        checkPerformance();
     }
 
     private static void checkPerformance() {
@@ -70,15 +70,17 @@ public class IndexH {
                 new TestCaseArray(3, new int[]{9, 7, 6, 2, 1}),
                 new TestCaseArray(1, new int[]{1, 3, 1}),
                 new TestCaseArray(3, new int[]{3, 0, 6, 1, 5}),
-                new TestCaseArray(7, new int[]{14, 5, 1, 1, 5, 4, 3, 11, 8, 7, 7, 6, 2, 6, 20, 3, 3, 18, 19, 0})
+                new TestCaseArray(7, new int[]{14, 5, 1, 1, 5, 4, 3, 11, 8, 7, 7, 6, 2, 6, 20, 3, 3, 18, 19, 0}),
+                new TestCaseArray(2, new int[]{25, 0, 0, 1, 80})
         );
 
         testCases.forEach((t) -> {
-            int result = getHIndex(t.getTarget());
+            int result = getHIndex2(t.getTarget());
             TestUtil.printTestResult(t.getExpected(), result);
         });
     }
 
+    // O(n2)
     private static int getHIndex(int[] citations) {
         int hIndex = 0;
         if (citations.length == 0) return hIndex;
@@ -99,9 +101,32 @@ public class IndexH {
         return hIndex;
     }
 
+    // O(n)
     private static int getHIndex2(int[] citations) {
-        int hIndex = 0;
-        return hIndex;
+        /* A HashMAp to get the numbers of papers with the same number of citations only when citation[i] > currentHIndex */
+        Map<Integer, Integer> map = new HashMap<>();
+        int currentHIndex = 0; // the hIndex of the first i terms.
+        int tempIndex = 0; // the number of papers with citations of at least "currentHIndex + 1"
+        for (int i = 0; i < citations.length; i++) {
+            /* adding only elements that are greater than the currentHIndex to the HashMap and their frequencies as keyValue*/
+            if (citations[i] > currentHIndex) {
+                if (map.containsKey(citations[i]))
+                    map.put(citations[i],
+                            1 + map.get(citations[i]));
+                else
+                    map.put(citations[i], 1);
+                /* increase the value of tempIndex by 1 since citations[i] is greater than currentHIndex*/
+                tempIndex += 1;
+            }
+            if (map.containsKey(currentHIndex)) {
+                tempIndex -= map.get(currentHIndex); //remove the number of papers of a citatoins of the value of currentHIndex
+                map.remove(currentHIndex);//ensuring that the map only contains key-value pair with key > currentHIndex
+            }
+
+            /* if there are at least "currentHIndex + 1" papers with at least "currentHIndex + 1" citations update currentHindex to the value of tempIndex */
+            if (tempIndex > currentHIndex) currentHIndex = tempIndex;
+        }
+        return currentHIndex;
     }
 
 }
