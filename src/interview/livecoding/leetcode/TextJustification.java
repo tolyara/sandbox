3fc61fe3d6a6f19cfa07e507432cc2a8d0045f34
Anatolii <anatolii.melchenko@academysmart.com>
2023-09-27 23:01:57 +0300

    fullJustify;

--- src/interview/livecoding/leetcode/TextJustification.java
+++ src/interview/livecoding/leetcode/TextJustification.java
@@ -19,11 +19,60 @@ import java.util.List;
  * Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
  * The input array words contains at least one word.
  *
+ * -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *
+ * Constraints:
+ *
+ * 1 <= words.length <= 300
+ * 1 <= words[i].length <= 20
+ * words[i] consists of only English letters and symbols.
+ * 1 <= maxWidth <= 100
+ * words[i].length <= maxWidth
+ *
+ * -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *
+ * Example 1:
+ *
+ * Input: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+ * Output:
+ * [
+ *    "This    is    an",
+ *    "example  of text",
+ *    "justification.  "
+ * ]
+ * Example 2:
+ *
+ * Input: words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
+ * Output:
+ * [
+ *   "What   must   be",
+ *   "acknowledgment  ",
+ *   "shall be        "
+ * ]
+ * Explanation: Note that the last line is "shall be    " instead of "shall     be", because the last line must be left-justified instead of fully-justified.
+ * Note that the second line is also left-justified because it contains only one word.
+ * Example 3:
+ *
+ * Input: words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], maxWidth = 20
+ * Output:
+ * [
+ *   "Science  is  what we",
+ *   "understand      well",
+ *   "enough to explain to",
+ *   "a  computer.  Art is",
+ *   "everything  else  we",
+ *   "do                  "
+ * ]
+ *
+ *
  */
 public class TextJustification {
 
     public static void main(String[] args) {
-
+        List<String> list = fullJustify(new String[]{"This", "is", "an", "example", "of", "text", "justification."}, 16);
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 
     // Thisisanexample
@@ -32,14 +81,27 @@ public class TextJustification {
 
     // Whatmustbeacknowledgmentshallbe
 
-    public List<String> fullJustify(String[] words, int maxWidth) {
+    // understandwell
+    // everythingelsewe
+
+    public static List<String> fullJustify(String[] words, int maxWidth) {
         List<String> result = new ArrayList<>();
 
-        int totalLength = 0;
+        StringBuilder lineBuilder = new StringBuilder();
         for (int i = 0; i < words.length; i++) {
-            totalLength += words[i].length();
+            if (lineBuilder.length() <= maxWidth) {
+                lineBuilder.append(lineBuilder.length() == 0 ? "" : " ");
+                lineBuilder.append(words[i]);
+            } else {
+                result.add(lineBuilder.toString());
+                lineBuilder = new StringBuilder();
+                lineBuilder.append(lineBuilder.length() == 0 ? "" : " ");
+                lineBuilder.append(words[i]);
+            }
+            if (i == words.length - 1) {
+                result.add(lineBuilder.toString());
+            }
         }
-
         return result;
     }
 
