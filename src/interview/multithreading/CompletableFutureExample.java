a6b5eefff1d526ad69a77b90230eafe47c40381a
Anatolii <anatolii.melchenko@academysmart.com>
2023-10-05 02:08:19 +0300

    CompletableFutureExample 2;

--- src/interview/multithreading/CompletableFutureExample.java
+++ src/interview/multithreading/CompletableFutureExample.java
@@ -7,13 +7,22 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/**
+ *
+ * https://www.baeldung.com/java-completablefuture
+ *
+ * https://www.callicoder.com/java-8-completablefuture-tutorial/
+ *
+ */
 public class CompletableFutureExample {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        doAsSimpleFuture();
+//        doAsSimpleFuture();
+//        doThenRun();
+        doThenCompose();
     }
 
-    public static Future<String> doAsSimpleFuture() throws ExecutionException, InterruptedException {
+    private static void doAsSimpleFuture() throws ExecutionException, InterruptedException {
         CompletableFuture<String> completableFuture = new CompletableFuture<>();
 
         Executors.newCachedThreadPool().submit(() -> {
@@ -24,8 +33,39 @@ public class CompletableFutureExample {
 
         String result = completableFuture.get();
         TestUtil.printTestResult("Hello", result);
+    }
 
-        return completableFuture;
+    private static void doThenRun() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> completableFuture
+                = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Computation started.");
+            sleepForAwhile();
+            return "Hello";
+        });
+
+        CompletableFuture<Void> future = completableFuture
+                .thenRun(() -> System.out.println("Computation finished."));
+
+        future.get();
+    }
+
+    private static void doThenCompose() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> completableFuture
+                = CompletableFuture.supplyAsync(() -> {
+                    sleepForAwhile();
+                    return "Hello";
+                })
+                .thenCompose(s -> CompletableFuture.supplyAsync(() -> s + " World"));
+
+        TestUtil.printTestResult("Hello World", completableFuture.get());
+    }
+
+    private static void sleepForAwhile() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
