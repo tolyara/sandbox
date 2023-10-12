8209d253480cea8fbfdfed97f7d87d5dd83c1e37
Anatolii <anatolii.melchenko@academysmart.com>
2023-10-13 00:38:50 +0300

    SubstringWithConcatenationOfAllWords skeleton;

---
+++ src/interview/livecoding/fortest/TestCaseArrayStrReturnsListOfInt.java
@@ -0,0 +1,11 @@
+package interview.livecoding.fortest;
+
+import java.util.List;
+
+public class TestCaseArrayStrReturnsListOfInt extends TestCase<List<Integer>, String[]> {
+
+    public TestCaseArrayStrReturnsListOfInt(List<Integer> expected, String[] target) {
+        super(expected, target);
+    }
+
+}
