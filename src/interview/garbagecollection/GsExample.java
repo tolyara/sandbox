ab048fa609a5b27d27b52720395fc410ecac5809
Anatolii <anatolii.melchenko@academysmart.com>
2023-11-26 02:53:10 +0200

    GsExample 2;

--- src/interview/garbagecollection/GsExample.java
+++ src/interview/garbagecollection/GsExample.java
@@ -3,12 +3,24 @@ package interview.garbagecollection;
 public class GsExample {
 
     public static void main(String[] args) throws Exception {
-        islandsOfIsolation();
+        referToAnotherObject();
+//        islandsOfIsolation();
+    }
+
+    private static void referToAnotherObject() {
+        GsTest str1 = new GsTest("111");
+        GsTest str2 = new GsTest("222");
+        //String object referred by str1 is not eligible for GC yet
+
+        str1 = str2;
+        /* Now the str1 variable referes to the String object "Another String" and the object "Garbage collected after use" is not referred by any variable and hence is eligible for GC */
+
+        Runtime.getRuntime().gc();
     }
 
     private static void islandsOfIsolation() throws InterruptedException {
-        GsTest gc1 = new GsTest();
-        GsTest gc2 = new GsTest();
+        GsTest gc1 = new GsTest("111");
+        GsTest gc2 = new GsTest("222");
         gc1.g = gc2; //gc1 refers to gc2
         gc2.g = gc1; //gc2 refers to gc1
 
@@ -29,9 +41,17 @@ class GsTest {
 
     GsTest g;
 
+    String text;
+
+    public GsTest() {}
+
+    public GsTest(String text) {
+        this.text = text;
+    }
+
     @Override
     protected void finalize() throws Throwable {
-        System.out.println(this + " finalized");
+        System.out.println((this.text != null ? text : this) + " finalized");
 //        super.finalize();
     }
 
