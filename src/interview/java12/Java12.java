3c3a7d5a1f1c6b749ff00b8ebe7bd77803c9e6c0
Anatolii <anatolii.melchenko@academysmart.com>
2024-03-09 14:29:45 +0200

    Java12 2;

--- src/interview/java12/Java12.java
+++ src/interview/java12/Java12.java
@@ -6,7 +6,8 @@ package interview.java12;
 public class Java12 {
 
     public static void main(String[] args) {
-        indentation();
+//        indentation();
+        transform();
     }
 
     private static void indentation() {
@@ -22,8 +23,37 @@ public class Java12 {
             If the parameter is less than zero, it removes spaces from the begging of each line.
             If a given line does not contain sufficient white space, then all leading white space characters are removed.
         */
-        text = text.indent(-10);
+//        text = text.indent(-10);
+//        System.out.println(text);
+
+        text = text.indent(-3);
         System.out.println(text);
     }
 
+    private static void transform() {
+        String text = "Baeldung";
+
+//        String transformed = text.transform(value ->
+//                new StringBuilder(value).reverse().toString()
+//        );
+        CustomString transformed = text.transform(CustomString::new);
+
+        System.out.println(transformed);
+    }
+
+}
+
+class CustomString {
+    private final String customValue;
+
+    CustomString(String customValue) {
+        this.customValue = customValue;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomString{" +
+                "customValue='" + customValue + '\'' +
+                '}';
+    }
 }
