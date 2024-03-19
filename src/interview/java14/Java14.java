cd3d7538fb884e92fc5fe0b40612f4626c11fbc9
Anatolii <anatolii.melchenko@academysmart.com>
2024-03-19 16:20:18 +0200

    Java14 2;

--- src/interview/java14/Java14.java
+++ src/interview/java14/Java14.java
@@ -1,9 +1,13 @@
 package interview.java14;
 
+/**
+ * https://www.baeldung.com/java-14-new-features
+ */
 public class Java14 {
 
     public static void main(String[] args) {
-        switchExpressions();
+//        switchExpressions();
+        textBlocks();
     }
 
     private static void switchExpressions() {
@@ -18,6 +22,22 @@ public class Java14 {
         };
 
         System.out.println(isTodayHoliday);
+    }
+
+    /*
+        In second preview, text blocks now have two new escape sequences:
+
+        \: to indicate the end of the line, so that a new line character is not introduced
+        \s: to indicate a single space
+     */
+    private static void textBlocks() {
+//        String multiline = "A quick brown fox jumps over a lazy dog; the lazy dog howls loudly.";
+
+        String multiline = """
+                A quick brown fox jumps over a lazy dog; \
+                the lazy dog howls loudly.""";
+
+        System.out.println(multiline);
     }
 
 }
